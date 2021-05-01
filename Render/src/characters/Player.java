ecd68ba9e4252548f23efaec41d08132b9f191fc
GuillaumeChamp <80750657+GuillaumeChamp@users.noreply.github.com>
2021-05-01 11:27:33 +0200

    Create the branche
    Uploading the Level 0 without uptdate the floor

---
+++ Render/src/characters/Player.java
@@ -0,0 +1,27 @@
+package characters;
+
+import image_define.ExtendImage.PlayerSkin;
+import image_define.Level;
+
+public class Player {
+    public PlayerSkin skin;
+    public Level location;
+    //public State state; to add statue to the player (example : frozen,slow)
+    public boolean onFireSide = true;
+    public boolean nextLevel = false;
+
+    public Player(int x, int y, int width, int height, int mass, Level currentLevel){
+        skin = new PlayerSkin(x, y, width, height);
+        skin.setMass(mass);
+        skin.setDuration(0.1);
+        skin.setPosition(x, currentLevel.getGround((double)x) - height);
+        location = currentLevel;
+    }
+
+    public void updateSkin(double time) {
+        this.skin.update(time, this.location);
+        if (this.skin.getPositionX() >= this.location.getSizeX() - 1.1 * this.skin.getWidth()) {
+            this.nextLevel = true;
+        }
+    }
+}
---
+++ Render/src/image_define/ExtendImage/PlayerSkin.java
@@ -0,0 +1,71 @@
+package image_define.ExtendImage;
+
+import javafx.scene.image.Image;
+import image_define.Level;
+import image_define.MovingAnimatedImage;
+
+public class PlayerSkin extends MovingAnimatedImage {
+    public Image playerLeft[];
+    public Image playerRight[];
+    public Image playerStopped[];
+
+    public PlayerSkin(int x, int y, int w, int h) {
+        super(x, y, w, h);
+        this.playerLeft = new Image[2];
+        this.playerRight = new Image[2];
+        this.playerStopped = new Image[1];
+        for (int i=0;i<2;i++) playerLeft[i]=new Image("resources/player/left"+(i+1)+".png");
+        for (int i=0;i<2;i++) playerRight[i]=new Image("resources/player/right" +(i+1)+".png");
+        for (int i=0;i<1;i++) playerStopped[i]=new Image("resources/player/mid.png");
+        this.setFrames(playerStopped);
+    }
+
+    public void update(double time, Level location) {
+        Image[] left = playerLeft;
+        Image[] right = playerRight;
+        Image[] stopped = playerStopped;
+        double oldX = positionX;
+        double oldY = positionY;
+
+        accelerationX = (forceX-velocityX/friction)/mass;
+        accelerationY = (forceY+gravity)/mass;
+
+        velocityX = velocityX+accelerationX;
+        velocityY = velocityY+accelerationY;
+
+        positionY += velocityY;
+        positionX += velocityX;
+        if (positionY<0) {
+            positionY=0;
+            velocityY=-velocityY/5;
+        }
+        if (positionY > location.getGround(this.getPositionX()) - this.getHeight()) {
+            positionY = location.getGround(this.getPositionX()) - this.getHeight();
+            velocityY=-velocityY/5;
+        }
+        if (positionX<0) {
+            positionX=0;
+            velocityX=-velocityX/5;
+        }
+        if (positionX > location.getSizeX() - this.getWidth()) {
+            positionX = location.getSizeX() - this.getWidth();
+            velocityX=-velocityX/5;
+        }
+        // unable to climb without jumping.
+        if ((int)oldY > location.getGround(positionX) - this.getHeight()) {
+            positionX = oldX;
+            positionY = oldY;
+            velocityX = 0;
+        }
+
+        if (Math.abs(velocityX) < 0.2) {
+            velocityX=0;
+            this.setFrames(stopped);
+            positionY = location.getGround(positionX) - this.getHeight();
+        } else if (velocityX < 0) {
+            this.setFrames(left);
+        } else {
+            this.setFrames(right);
+        }
+    }
+}
