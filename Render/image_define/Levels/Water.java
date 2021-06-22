bfd45079f417a27a7abaa4bb5593e815338d27e0
GuillaumeC_pc1 <guillaume.champtoussel@ensea.fr>
2021-06-22 12:23:03 +0200

    adding the end of the set

--- Render/image_define/Levels/Water.java
+++ Render/image_define/Levels/Water.java
@@ -3,10 +3,9 @@ package image_define.Levels;
 import javafx.scene.image.Image;
 
 public class Water extends Block{
-    Image skin2;
     public Water(double x,double y,double width,double height){
         super( x, y, width, height,"null");
-        //skin = new Image(path_to)
-        //skin2 = new Image(path_to)
+        skin1 = new Image("resources/Level/ice1.png",width,height,false,true);
+        skin2 = new Image("resources/Level/ice2.png",width,height,false,true);
     }
 }
